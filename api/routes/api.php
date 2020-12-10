4037014d960028e317ceed1541d0f577c7538635
Eduardo Salazar <eduardosalazar89@hotmail.es>
2020-12-10 14:08:45 -0600

    Configure reverse proxy for keep host port
    enable api info and version

--- api/routes/api.php
+++ api/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use \App\Http\Controllers\ApiController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', [ApiController::class, 'redirect']);
+Route::group(['prefix' =>'v1'], function () {
+    Route::get('/', [ApiController::class, 'index'])->name('api:info');
+
+    Route::middleware('auth:api')->get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
